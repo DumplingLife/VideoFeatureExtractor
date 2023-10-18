d3afa3a83b4de837db704031668e63128ab6f9ae
Jason Cheng <dumplinglife7584@gmail.com>
2023-10-18 18:36:24 -0400

    Update combine_features.py

--- combine_features.py
+++ combine_features.py
@@ -7,8 +7,13 @@ import os
 name = sys.argv[1]
 directory_path = f"features_segmented/{name}"
 
-# Get list of all .npy files in the directory
-file_paths = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith('.npy')]
+def sort_files(file):
+    return 
+
+file_paths = sorted(
+    [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith('.npy')],
+    key=lambda file: int(file.split('.')[0])
+    )
 
 arrays = []
 for file_path in file_paths:
