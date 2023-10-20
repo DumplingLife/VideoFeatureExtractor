1954f65069d1b5e8f263e61ff7c5f61280b28676
Jason Cheng <dumplinglife7584@gmail.com>
2023-10-20 17:20:38 -0400

    Update get_features.sh

--- get_features.sh
+++ get_features.sh
@@ -7,6 +7,12 @@ fi
 
 NAME="$1"
 
+mkdir videos/${NAME}
+
+ffmpeg -i videos_raw/${NAME}.avi -vf "fps=10,scale=224:224" -c:v libx264 -preset fast -c:a copy -map 0 -segment_time 320 -f segment videos/${NAME}/%d.avi
+
+mkdir features_segmented/${NAME}
+
 python preprocess_generate_csv.py --csv=input.csv --video_root_path "./videos/${NAME}" --feature_root_path "./features_segmented/${NAME}" --csv_save_path .
 
 python extract.py --csv=./input.csv --type=s3dg --batch_size=64 --num_decoding_thread=1
